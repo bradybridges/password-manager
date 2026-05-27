e37b528c37d869c2df47f2ba12ee1d700f608831
brady bridges <bradyjbridges@gmail.com>
2026-05-26 20:19:49 -0500

    Add frame to allow users to login to a vault

--- frames/login.py
+++ frames/login.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox, ttk
 
-from frames.vault import Vault
+from utils.vault_manager import VaultNotFoundError, VaultWrongPasswordError
 
 
 class Login(ttk.Frame):
@@ -9,31 +9,76 @@ class Login(ttk.Frame):
         super().__init__(container)
         self.controller = controller
 
-        # Password label and input
-        def validate_password(var, index, mode):
-            current_entry = self.password_text.get()
+        self._build_no_vaults_ui()
+        self._build_unlock_ui()
 
-            if current_entry:
-                self.button.state(["!disabled"])
+    def load(self):
+        vaults = self.controller.vault_manager.list()
+        if vaults:
+            self._show_unlock(vaults)
+        else:
+            self._show_no_vaults()
+
+    # --- no-vaults UI ---
+
+    def _build_no_vaults_ui(self):
+        self.no_vaults_frame = ttk.Frame(self)
+        ttk.Label(self.no_vaults_frame, text="No vaults found.").pack(pady=(60, 10))
+        ttk.Button(
+            self.no_vaults_frame,
+            text="Create Your First Vault",
+            command=lambda: self.controller.show_frame("CreateVault"),
+        ).pack(ipadx=5, ipady=5)
+
+    # --- unlock UI ---
+
+    def _build_unlock_ui(self):
+        self.unlock_frame = ttk.Frame(self)
+
+        ttk.Label(self.unlock_frame, text="Vault").pack(pady=(40, 2))
+        self.vault_var = tk.StringVar()
+        self.vault_dropdown = ttk.Combobox(
+            self.unlock_frame, textvariable=self.vault_var, state="readonly"
+        )
+        self.vault_dropdown.pack(pady=2, padx=5)
+
+        def validate(_var, _index, _mode):
+            if self.password_text.get():
+                self.unlock_button.state(["!disabled"])
             else:
-                self.button.state(["disabled"])
+                self.unlock_button.state(["disabled"])
 
-        # Label
-        self.password_label = ttk.Label(self, text="Master Password")
-        self.password_label.pack(pady=2)
-
-        # Entry observer
+        ttk.Label(self.unlock_frame, text="Master Password").pack(pady=(8, 2))
         self.password_text = tk.StringVar()
-        self.password_text.trace_add("write", validate_password)
+        self.password_text.trace_add("write", validate)
+        ttk.Entry(self.unlock_frame, show="*", textvariable=self.password_text).pack(pady=2, padx=5)
 
-        # Password entry
-        self.password_entry = ttk.Entry(self, show="*", textvariable=self.password_text)
-        self.password_entry.pack(pady=5, padx=5)
+        self.unlock_button = ttk.Button(self.unlock_frame, text="Unlock", command=self.handle_click)
+        self.unlock_button.state(["disabled"])
+        self.unlock_button.pack(pady=10, ipadx=5, ipady=5)
 
-        # Button
-        self.button = ttk.Button(self, text="Unlock", command=self.handle_click)
-        self.button.state(["disabled"])
-        self.button.pack(ipadx=5, ipady=5)
+    def _show_unlock(self, vaults):
+        self.no_vaults_frame.place_forget()
+        self.vault_dropdown["values"] = vaults
+        if vaults:
+            self.vault_var.set(vaults[0])
+        self.password_text.set("")
+        self.unlock_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+    def _show_no_vaults(self):
+        self.unlock_frame.place_forget()
+        self.no_vaults_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+    # --- actions ---
 
     def handle_click(self):
-        self.controller.show_frame(Vault)
+        name = self.vault_var.get()
+        password = self.password_text.get()
+        try:
+            credentials = self.controller.vault_manager.open(name, password)
+            self.password_text.set("")
+            self.controller.show_frame("Vault", credentials=credentials, vault_name=name, password=password)
+        except VaultWrongPasswordError:
+            messagebox.showerror("Incorrect Password", "The password you entered is incorrect.")
+        except VaultNotFoundError:
+            messagebox.showerror("Vault Not Found", f'No vault named "{name}" was found.')
