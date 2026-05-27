56481eb0aab1088ed785fb03511a3939d8505ed4
brady bridges <bradyjbridges@gmail.com>
2026-05-26 20:20:49 -0500

    Hook up login, create vault, and vault frames. Add import/export file menu options

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import filedialog, messagebox, ttk
 
+from frames.create_vault import CreateVault
 from frames.login import Login
 from frames.vault import Vault
+from utils.vault_manager import VaultCorruptError, VaultManager
 
 
 class App(tk.Tk):
@@ -19,7 +21,6 @@ class App(tk.Tk):
         window_x_position = int((screen_width / 2) - (window_width / 2))
         window_y_position = int((screen_height / 2) - (window_height / 2))
 
-        # Set window dimensions
         window_geometry = (
             f"{window_width}x{window_height}+{window_x_position}+{window_y_position}"
         )
@@ -29,9 +30,15 @@ class App(tk.Tk):
         self.geometry(window_geometry)
         self.resizable(False, False)
 
+        # Init vault manager
+        self.vault_manager = VaultManager()
+
         # Menubar
         menubar = tk.Menu(self)
         file_menu = tk.Menu(menubar, tearoff=0)
+        file_menu.add_command(label="Import Vault", command=self.import_vault)
+        file_menu.add_command(label="Export Vault", command=self.export_vault)
+        file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.quit)
         menubar.add_cascade(label="File", menu=file_menu)
         self.config(menu=menubar)
@@ -42,16 +49,53 @@ class App(tk.Tk):
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
 
+        # Frames setup
         self.frames = {}
-        for FrameClass in (Login,Vault):
+        for name, FrameClass in (("Login", Login), ("CreateVault", CreateVault), ("Vault", Vault)):
             frame = FrameClass(self.container, self)
-            self.frames[FrameClass] = frame
+            self.frames[name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(Login)
+        self.show_frame("Login")
 
-    def show_frame(self, frame_class):
-        self.frames[frame_class].tkraise()
+    def show_frame(self, name, **kwargs):
+        frame = self.frames[name]
+        if kwargs:
+            frame.load(**kwargs)
+        elif hasattr(frame, "load"):
+            frame.load()
+        frame.tkraise()
+
+    def import_vault(self):
+        path = filedialog.askopenfilename(
+            title="Import Vault",
+            filetypes=[("Vault files", "*.vault")],
+        )
+        if not path:
+            return
+        try:
+            imported_name = self.vault_manager.import_vault(path)
+            messagebox.showinfo("Vault Imported", f'Vault "{imported_name}" imported successfully.')
+            if self.frames["Login"].winfo_ismapped() or True:
+                self.show_frame("Login")
+        except VaultCorruptError:
+            messagebox.showerror("Import Failed", "The selected file is not a valid vault.")
+
+    def export_vault(self):
+        vault_frame = self.frames["Vault"]
+        if not vault_frame.vault_name:
+            messagebox.showinfo("No Vault Open", "Open a vault first before exporting.")
+            return
+        path = filedialog.asksaveasfilename(
+            title="Export Vault",
+            initialfile=f"{vault_frame.vault_name}.vault",
+            defaultextension=".vault",
+            filetypes=[("Vault files", "*.vault")],
+        )
+        if not path:
+            return
+        self.vault_manager.export(vault_frame.vault_name, path)
+        messagebox.showinfo("Vault Exported", f'Vault exported to "{path}".')
 
 
 if __name__ == "__main__":
