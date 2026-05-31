9bd66033ba22167e7e8c2c87062e84834d3b2f15
brady bridges <bradyjbridges@gmail.com>
2026-05-31 15:17:54 -0500

    Add utility to get window information
    May be needed in the future in multiple areas. Utils returns current
    window dimensions and coordinates to make centering windows more
    straight-forward

---
+++ utils/window.py
@@ -0,0 +1,24 @@
+from tkinter import Tk, Toplevel
+
+
+def get_window_info(
+    Tk: Tk | Toplevel, window_width: int, window_height: int
+) -> dict[str, int]:
+    # Screen dimensions
+    screen_width = Tk.winfo_screenwidth()
+    screen_height = Tk.winfo_screenheight()
+
+    # Center of screen coordinates
+    screen_x_center = int(screen_width / 2)
+    screen_y_center = int(screen_height / 2)
+    window_x_center = int(screen_x_center - (window_width / 2))
+    window_y_center = int(screen_y_center - (window_height / 2))
+
+    return {
+        "screen_width": screen_width,
+        "screen_height": screen_width,
+        "screen_x_center": screen_x_center,
+        "screen_y_center": screen_y_center,
+        "window_x_center": window_x_center,
+        "window_y_center": window_y_center,
+    }
